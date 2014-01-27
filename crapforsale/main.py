10e70491bfcfe7845a7f3697fb51814999147f8a
crccheck <c@crccheck.com>
2014-01-26 18:16:07 -0600

    update to render a template

--- crapforsale/main.py
+++ crapforsale/main.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 from project_runpy import env
 
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def homepage():
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     if env.get('DEBUG'):
