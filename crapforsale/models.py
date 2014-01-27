432bbe0e09e91c013c6a040afce15ebdd35aca0e
crccheck <c@crccheck.com>
2014-01-27 00:09:39 -0600

    add a cheapo model

---
+++ crapforsale/models.py
@@ -0,0 +1,16 @@
+"""
+from crapforsale import db
+db.create_all()
+"""
+from . import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, email):
+        self.email = email
+
+    def __repr__(self):
+        return self.email
