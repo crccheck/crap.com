2ba4e17c9d4c8efd3886c039e603da18ae9df423
crccheck <c@crccheck.com>
2014-01-29 23:14:05 -0600

    please auto rollback. pleeeeaase

--- crapforsale/__init__.py
+++ crapforsale/__init__.py
@@ -5,6 +5,10 @@ from playhouse.postgres_ext import PostgresqlExtDatabase
 
 app = Flask(__name__)
 app.config.from_object('crapforsale.config')
-db = PostgresqlExtDatabase(**app.config['DATABASE'])
+db = PostgresqlExtDatabase(
+        # let me be lazier
+        autocommit=True,
+        autorollback=True,
+        **app.config['DATABASE'])
 
 from .views import *
