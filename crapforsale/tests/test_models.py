1bde9dbc3ff54c3314e9e2a7559f904c5b97fe66
crccheck <c@crccheck.com>
2014-01-31 00:54:54 -0600

    add test coverage

---
+++ crapforsale/tests/test_models.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+
+from ..models import (
+    Comparison,
+    Item,
+)
+
+class ComparisonTest(TestCase):
+    def test_it_works(self):
+        # TODO after I'm ok with touching the database in tests
+        pass
+
+
---
+++ crapforsale/tests/test_utils.py
@@ -0,0 +1,25 @@
+import random
+
+from unittest import TestCase
+import mock
+
+from .. import utils
+
+
+class ParseUrlTest(TestCase):
+    def test_stuff_happens(self):
+        n_items = random.randint(5, 10)
+        mock_gsheet = mock.MagicMock()
+        mock_gsheet.return_value = range(n_items)
+        mock_comparison = mock.MagicMock()
+        mock_item = mock.MagicMock()
+
+        with mock.patch.multiple(utils,
+                GSpreadsheet=mock_gsheet,
+                Comparison=mock_comparison,
+                Item=mock_item):
+            utils.parse_url('foobar')
+
+        mock_gsheet.assert_called_once_with('foobar')
+        self.assertEqual(mock_comparison.call_count, 1)
+        self.assertEqual(mock_item.call_count, n_items)
