f2e83e7205e99165bb251c52737bb4e1dfa49708
crccheck <c@crccheck.com>
2014-02-02 01:34:24 -0600

    handle double adding spreadsheet sorta

--- craptobuy/utils.py
+++ craptobuy/utils.py
@@ -6,10 +6,14 @@ from .models import Comparison, Item
 def parse_url(url):
     sheet = GSpreadsheet(url)
 
-    comparison = Comparison.create(
-        url=url,
-        name=sheet.feed.title.text,
-    )
+    try:
+        comparison = Comparison.get(url=url)
+        Item.delete().where(Item.comparison == comparison)
+    except Comparison.DoesNotExist:
+        comparison = Comparison.create(
+            url=url,
+            name=sheet.feed.title.text,
+        )
     for row in sheet:
         data = row.copy()  # to get this JSON serializable
         Item.create(comparison=comparison, data=data)
