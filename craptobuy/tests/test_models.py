835acb2a85624769750b213db22b43d2f48722ca
crccheck <c@crccheck.com>
2014-02-04 21:53:37 -0600

    add test to verify .price custom property

--- craptobuy/tests/test_models.py
+++ craptobuy/tests/test_models.py
@@ -1,13 +1,28 @@
+import datetime
 from unittest import TestCase
 
 from ..models import (
-    Comparison,
-    Item,
+    AmazonProduct,
+    PriceHistory,
 )
 
-class ComparisonTest(TestCase):
-    def test_it_works(self):
-        # TODO after I'm ok with touching the database in tests
-        pass
 
+class AmazonProductTest(TestCase):
+    def test_price_gets_right_value(self):
+        asin = AmazonProduct.create(asin='foo')
+        PriceHistory.create(asin=asin, price=10, currency='foo',
+                retrieved=datetime.datetime.now())
+        self.assertEqual(asin.price, 10)
 
+        # assert an even more "current" price is found
+        PriceHistory.create(asin=asin, price=11, currency='foo',
+                retrieved=datetime.datetime.now() + datetime.timedelta(hours=1))
+        self.assertEqual(asin.price, 11)
+
+        # assert an new old price does has no influence
+        PriceHistory.create(asin=asin, price=12, currency='foo',
+                retrieved=datetime.datetime.now() - datetime.timedelta(hours=1))
+        self.assertEqual(asin.price, 11)
+
+    # WISHLIST
+    # def test_price_____not_found(self)
