380d32fb42a51950affa6ea23cce6f9a82e58df9
crccheck <c@crccheck.com>
2014-02-10 23:10:06 -0600

    add stub for amazon invoke task
    using invoke for now, celery later? either way, maybe invoke will be the
    runner.

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,9 +1,10 @@
 from invoke import Collection
 
-from . import db, repl, sass, test
+from . import amazon, db, repl, sass, test
 
 
 ns = Collection()
+ns.add_collection(amazon)
 ns.add_collection(db)
 ns.add_collection(repl)
 ns.add_collection(sass)
